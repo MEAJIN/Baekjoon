bf8b5a90245f13bb22c53f8eb8c034433896e725
MEAJIN <75716255+MEAJIN@users.noreply.github.com>
2021-05-31 23:12:59 +0900

    Update 2577. 숫자의 개수.py

--- L5/2577. 숫자의 개수.py
+++ L5/2577. 숫자의 개수.py
@@ -1,3 +1,4 @@
+#1
 A = int(input())
 B = int(input())
 C = int(input())
@@ -7,3 +8,15 @@ all_multiplication_str = str(A*B*C)
 for i in range(10):
   all_multiplication_count = all_multiplication_str.count(str(i))
   print(all_multiplication_count)
+
+  #2
+total = 1
+for _ in range(3):
+    i = int(input())
+    total *= i  # 3개의 정수를 곱함
+    
+total_str = str(total)  # 숫자를 str타입으로 변환
+
+for num in range(10):  # 0부터 9까지
+    num_count = total_str.count(str(num))
+    print(num_count)
