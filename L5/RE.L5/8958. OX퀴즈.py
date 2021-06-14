e5b7f9bc5af3643784c5bc2021d7769890bd93fa
MEAJIN <75716255+MEAJIN@users.noreply.github.com>
2021-06-14 15:41:20 +0900

    Create 8958. OX퀴즈.py

---
+++ L5/RE.L5/8958. OX퀴즈.py
@@ -0,0 +1,23 @@
+#1
+n = int(input())
+quiz = []
+for i in range(n):
+  quiz.append(input())
+  a = str(quiz)
+
+#1.1
+n = int(input())
+for i in range(n):
+  quiz = list(input())
+
+##
+score = 0 
+total_s = 0
+for i in a:
+  if i == 'o':
+    score += 1
+    total_s += score
+  else:
+    score = 0
+
+print(total_s)
