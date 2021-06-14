321fe6c6e593c6e13507e24af685db66cc27f693
MEAJIN <75716255+MEAJIN@users.noreply.github.com>
2021-06-14 16:10:55 +0900

    Update 8958. OX퀴즈.py

--- L5/RE.L5/8958. OX퀴즈.py
+++ L5/RE.L5/8958. OX퀴즈.py
@@ -1,16 +1,7 @@
-#1
-n = int(input())
-quiz = []
-for i in range(n):
-  quiz.append(input())
-  a = str(quiz)
-
-#1.1
 n = int(input())
 for i in range(n):
   quiz = list(input())
 
-##
 score = 0 
 total_s = 0
 for i in a:
