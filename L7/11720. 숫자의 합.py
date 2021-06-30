38d39bf85ce0084cda006eb34f6efe2fe9b82f2f
MEAJIN <75716255+MEAJIN@users.noreply.github.com>
2021-06-30 20:58:18 +0900

    Update 11720. 숫자의 합.py

--- L7/11720. 숫자의 합.py
+++ L7/11720. 숫자의 합.py
@@ -1,3 +1,4 @@
+#1
 n = int(input())
 nums = input()
 nums_list = []
@@ -6,3 +7,8 @@ for i in range(n):
   nums_list.append(int(nums[i]))
 
 print(sum(nums_list))
+
+#2
+n = int(input())
+
+print(sum(map(int,input())))
