6cff297ff5205a0b900bbb7761758a391bb06a28
메진 <a5341663@naver.com>
2021-07-21 13:04:56 +0900

    Update 2941. 크로아티아 알파벳.py

--- L7/2941. 크로아티아 알파벳.py
+++ L7/2941. 크로아티아 알파벳.py
@@ -1,7 +1,10 @@
-a = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-alpha = input()
+c_a = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='] #crotia_alphabet
+word = input()
 
-for t in a:
-  alpha = alpha.replace(t, '*')
-  
-print(len(alpha))
+for i in c_a:
+	word = word.replace(i,'*')
+
+print(len(word))
+
+#replace는 문자열 자체를 변경하지 않고 바뀐 결과를 반환한다.
+#따라서, 바뀐 결과를 유지하고 싶다면 문자열이 저장된 변수에 재할당을 해야함.
