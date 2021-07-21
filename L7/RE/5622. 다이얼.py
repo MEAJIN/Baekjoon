bc748bf5af9cfbf5cf2a4c14c914f26105933cf5
메진 <a5341663@naver.com>
2021-07-21 10:50:21 +0900

    Update 5622. 다이얼.py

--- L7/RE/5622. 다이얼.py
+++ L7/RE/5622. 다이얼.py
@@ -1,8 +1,12 @@
-dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
-a = input()
-ret = 0
-for j in range(len(a)):
-    for i in dial:
-        if a[j] in i:
-            ret += dial.index(i)+3
-print(ret)
+dial = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+word  = input()
+
+sum = 0
+for i in range(len(word)):
+	for j in dial:
+		if word[i] in j:
+			sum += dial.index(j)+3
+
+print(sum)
+
+# if * in ** 를 사용 할 땐, *에 문자열이 와야한다. 
