c0bcfc51d55ed089951a0a8960bc074e3c4f4df5
메진 <a5341663@naver.com>
2021-07-29 18:21:29 +0900

    Create 10250.  ACM 호텔.py

---
+++ L8/10250.  ACM 호텔.py
@@ -0,0 +1,10 @@
+T=int(input())
+
+for i in range(T):
+    H, W, N = map(int, input().split())
+    dist = N//H+1
+    floor = N%H
+    if floor==0:
+        dist-=1
+        floor=H
+    print(floor*100+dist)
